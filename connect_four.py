c2e2e5bd0d358a94a7f5531cd0b6122097313a94
madeinuzza <rafaelagesing@gmail.com>
2026-01-13 11:39:13 +0100

    Second commit

--- connect_four.py
+++ connect_four.py
@@ -124,7 +124,7 @@ def empty_space_left_test(board):
 
 
 
-############################ Input data (?) ############################
+############################ Input data ############################
 
 
 # Connect Four is a game in which the players choose a color and then take turns dropping colored tokens into a six-row, seven-column 
